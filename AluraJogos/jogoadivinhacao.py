74425e720ecbbf3d3a28eca068bcf7c4b1c71bd1
arrozcomgado <82466956+arrozcomgado@users.noreply.github.com>
2022-05-12 13:48:10 -0300

    Update jogoadivinhacao.py

--- AluraJogos/jogoadivinhacao.py
+++ AluraJogos/jogoadivinhacao.py
@@ -10,16 +10,39 @@ print("*********************************")
 
 #Variaveis
 numero_secreto = random.randint(1, 101)
-tentativas = 5
+tentativas = 1
 rodadas = 1
 
-#stopwatch start
+#Difficuldade
+print("Escolha a dificuldade:")
+print("(1) Fácil (2) Médio (3) Difícil")
+while True:
+    try:
+        lvl_tentativas = int(input())
+        if lvl_tentativas >= 1 and lvl_tentativas <= 3:
+            pass
+        else:
+            print(colored("O número escrito não é aceito", 'red'))
+            break
+        if lvl_tentativas == 1:
+            tentativas = 20
+            break
+        elif lvl_tentativas == 2:
+            tentativas = 10
+            break
+        else:
+            tentativas = 5
+            break
+    except:
+        pass
+
+#cronometro start
 start = time.time()
 #check user input(float,int, string)
 while (rodadas <= tentativas) and True:
 
     print(colored("Total de tentativas: {} de {}".format(rodadas, tentativas), "cyan"))
-    chute = input("Digite um número: ",)
+    chute = input("Digite um número entre 1 e 100: ",)
     try:
         chute_int = int(chute)
         if chute_int < 1 or chute_int > 100:
@@ -51,15 +74,13 @@ while (rodadas <= tentativas) and True:
             print(colored("O número colocado não é valido","red"))
 
         except ValueError:
-            print(colored("Não foi detectado um número. Por favor escreva um número válido","red"))
+            print(colored("Não foi detectado um número. Por favor escreva um número válido", "red"))
 
-
-
-
-if tentativas == 5:
+if tentativas != -1:
     print(colored("Você perdeu!", "red"))
     end = time.time()
     print("O número secreto era:", numero_secreto)
     tempo = end - start
-    print("Tempo:",round(tempo, 2))
+    print("Tempo:", round(tempo, 2))
+
 
